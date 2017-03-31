e165f4776a4b58385c240bb72b81b396abdc1733
Hamza Hameed <hh0718@yahoo.com>
2017-03-30 20:36:09 -0500

    added main, import

--- Lab7/Matrix.java
+++ Lab7/Matrix.java
@@ -1,6 +1,69 @@
 import java.lang.Math;
+import java.io.*;
 
 public class Matrix {
+	
+	public static void main(String[] args){
+		
+		try{
+			FileReader reader = new FileReader("input.txt");
+			BufferedReader bufReader = new BufferedReader(reader);
+			FileWriter writer = new FileWriter("output.txt");
+			BufferedWriter bufWriter = new BufferedWriter(writer);
+			String currentLine;
+			
+			while((currentLine = bufReader.readLine()) != null){
+				if(currentLine.length() == 1 && Integer.parseInt(currentLine) == 0){
+					System.out.println("Done!");
+				}if(currentLine.length() == 1 && Integer.parseInt(currentLine) != 0){
+					int size = Integer.parseInt(currentLine);
+					double[][] matrix = new double[size][size];
+					System.out.println("M = ");
+					for(int i=0; i < size; i++){
+						String splitLine[] = bufReader.readLine().split(" ");
+						matrix[i] = new double[splitLine.length];
+						for(int j=0; j<size; j++){
+							matrix[i][j] = Integer.parseInt(splitLine[j]);
+						}
+					}
+					
+					for(int i=0; i< size; i++){
+						for(int j=0; j< size; j++){
+							bufWriter.write((int) matrix[i][j]);
+							bufWriter.write(" ");
+						}
+						bufWriter.newLine();
+					}
+					
+					//print array
+					for(int i=0; i< size; i++){
+						for(int j=0; j< size; j++){
+							System.out.print(matrix[i][j]);
+							System.out.print(" ");
+						}
+						System.out.println();
+					}
+					double det = determinant(size, matrix);
+					System.out.println("det(M) = " + det);
+					System.out.println("Minv =");
+					double[][] inv = new double[size][size];
+					inv = inverse(size, matrix);
+					for(int i=0; i< size; i++){
+						for(int j=0; j< size; j++){
+							System.out.print(inv[i][j]);
+							System.out.print(" ");
+						}
+						System.out.println();
+					}
+					
+				}
+			}
+		}catch(Exception e){
+			System.out.println("Error: File doesn't exist!");
+		}
+		
+	}
+	
 	double determinant(int size, double[][]matrix){
 		
 		double det = 0;
